8799b61c2731a340f4d3bf89d442b7ead6ea19c1
Md Samiul Huq <samiulg3@gmail.com>
2014-07-01 11:16:45 -0700

    Added twilio #

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -1,4 +1,10 @@
 class SendTextController < ApplicationController
   def send_text_message
+    number_to_send_to = params[:number_to_send_to]
+
+    twilio_sid = ""
+    twilio_token = "foober"
+    twilio_phone_number = "3477652775"
+
   end
 end

97610efb330fa34aec0f7de76a7b1d1386ee4710
Md Samiul Huq <samiulg3@gmail.com>
2014-07-01 11:50:03 -0700

    Added verifications controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :phone, :verified
   validates :name, presence: true
-  validates :email,:phone
+  validates :email,:phone, presence: true, uniqueness: true
 end

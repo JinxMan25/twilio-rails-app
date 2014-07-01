2a1734e9dff9fa30d381cbede0bd0f0b8b2b416e
Md Samiul Huq <samiulg3@gmail.com>
2014-07-01 11:19:29 -0700

    added SID and token

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -2,9 +2,11 @@ class SendTextController < ApplicationController
   def send_text_message
     number_to_send_to = params[:number_to_send_to]
 
-    twilio_sid = ""
-    twilio_token = "foober"
+    twilio_sid = "AC633748333235e905dd3fb0f904ad4b31"
+    twilio_token = "5a07271a4f44fdaee8ee43fe0a84de78"
     twilio_phone_number = "3477652775"
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid,twilio_token
 
   end
 end

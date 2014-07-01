fc95d9e863a0423ccdb7781f1a24e2f90cbc6138
Md Samiul Huq <samiulg3@gmail.com>
2014-07-01 11:07:39 -0700

    Added gem and created controller

---
+++ twilio/app/controllers/send_text_controller_controller.rb
@@ -0,0 +1,4 @@
+class SendTextControllerController < ApplicationController
+  def send_text_message
+  end
+end
---
+++ twilio/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twilio::Application.config.secret_token = '2e9dffd789fbbb8a778adeb4448cfb6d90fa796cf362e0db1e30eb8183c2b584f8f7530abe89bd13dd244fe773ee9527be74efdd3d82433b5bdfcc8d039e8d7c'

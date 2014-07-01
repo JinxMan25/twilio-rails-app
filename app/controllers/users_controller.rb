a835f07beb58ac82c2f9f7ea37f3b8fb6d4c5cd3
Md Samiul Huq <samiulg3@gmail.com>
2014-07-01 12:06:28 -0700

    Added forms for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      render text: "WE'll send you text man"
+    else
+      render :new
 end
